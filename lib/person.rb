1989a60ecdbcedb9979dc984013a069d7fdf6ab3
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-06-17 18:57:24 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+  
+  def name= (this_is_you)
+  @name = this_is_you
+  end
+  def name
+    @name
+  end
+  def job= (person_works_here)
+    @job = person_works_here
+  end 
+  def job
+    @job
+  end 
+end
