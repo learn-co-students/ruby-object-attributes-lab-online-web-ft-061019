514ea7e0fdc0112c8a02ac11f65fd41099add147
Adam E <adamjervin@gmail.com>
2019-06-28 20:11:46 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Dog 
+   
+  def name=(name)
+    @name =  name 
+
+  end 
+  def name 
+  @name
+end 
+fido = Dog.new 
+fido.name = "fido"
+
+def breed=(breed)
+  @breed = breed
+  
+end 
+def breed 
+  @breed 
+end 
+snoop = Dog.new 
+snoop.breed = "Beagle"
+end 
+
+
+
+
+
